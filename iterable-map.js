a58315746ea7bbda2f4af0c67bfb01d71d1ea3a3
rektide de la faye <rektide@voodoowarez.com>
2017-03-27 23:48:16 -0400

    Lazily iterate and map an iterable.

---
+++ iterable-map.js
@@ -0,0 +1,20 @@
+function iterableMap( mapFn){
+	return function( iterable){
+		var
+		  iterator= iterable[Symbol.iterator](),
+		  retVal= {
+			[Symbol.iterator](){ return this }
+			next(){
+				var val= iterator.next()
+				val.value= mapFn( val.value)
+				return val
+			}
+		  }
+		if( iterator.return){
+			retVal.return= iterator.return.bind( iterator)
+		}
+		if( iterator.throw){
+			retVal.throw= iterator.throw.bind( iterator)
+		}
+	}	
+}
